1af7994f5d3f49e375274d44e5d0c0b0d72ceacb
aansourav <aansourav@gmail.com>
2024-08-07 22:30:08 +0600

    "Added Header component to Document page"

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 
 const Document = () => {
     return (
         <div>
+            <Header />
             <Editor />
         </div>
     );
