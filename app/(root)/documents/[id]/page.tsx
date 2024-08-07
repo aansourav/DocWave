6362c24ae943719999e9359ad1129c03ae7037b2
aansourav <aansourav@gmail.com>
2024-08-08 00:59:13 +0600

    "Added Clerk integration, updated dependencies, and modified layout and document pages"

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,20 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
     return (
         <div>
             <Header />
+            <div className="flex w-fit items-center justify-center gap-2">
+                <p className="document-title">This is title</p>
+            </div>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
             <Editor />
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { ReactNode } from "react";
@@ -16,16 +18,26 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <head />
-            <body
-                className={cn(
-                    "min-h-screen font-sans antialiased",
-                    fontSans.variable
-                )}
-            >
-                {children}
-            </body>
-        </html>
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+                variables: {
+                    colorPrimary: "#3371FF",
+                    fontSize: "16px",
+                },
+            }}
+        >
+            <html lang="en" suppressHydrationWarning>
+                <head />
+                <body
+                    className={cn(
+                        "min-h-screen font-sans antialiased",
+                        fontSans.variable
+                    )}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
