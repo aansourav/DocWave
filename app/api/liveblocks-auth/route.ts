d57817758a8d7bc03a7478aa7f6cf8f2efa941b7
aansourav <aansourav@gmail.com>
2024-08-08 09:48:11 +0600

    "Refactor Liveblocks auth route to use Clerk user data and identifyUser method"

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,21 +1,35 @@
 import { liveblocks } from "@/lib/liveblocks";
+import { getUserColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export async function POST(request: Request) {
-    // Get the current user from your database
-    const user = __getUserFromDB__(request);
+    const clerkUser = await currentUser();
 
-    // Start an auth session inside your endpoint
-    const session = liveblocks.prepareSession(
-        user.id,
-        { userInfo: user.metadata } // Optional
+    if (!clerkUser) redirect("/sign-in");
+
+    const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
+
+    // Get the current user from your database
+    const user = {
+        id,
+        info: {
+            id,
+            name: `${firstName} ${lastName}`,
+            email: emailAddresses[0].emailAddress,
+            avatar: imageUrl,
+            color: getUserColor(id),
+        },
+    };
+
+    // Identify the user and return the result
+    const { status, body } = await liveblocks.identifyUser(
+        {
+            userId: user.info.email,
+            groupIds: [],
+        },
+        { userInfo: user.info }
     );
 
-    // Use a naming pattern to allow access to rooms with wildcards
-    // Giving the user read access on their org, and write access on their group
-    session.allow(`${user.organization}:*`, session.READ_ACCESS);
-    session.allow(`${user.organization}:${user.group}:*`, session.FULL_ACCESS);
-
-    // Authorize the user and return the result
-    const { status, body } = await session.authorize();
     return new Response(body, { status });
 }
