89750794b39da88960d560cb2f16784269fa6f20
aansourav <aansourav@gmail.com>
2024-08-12 03:32:54 +0600

    nextjs config conflict with sentry deployment issue fixed

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,15 +5,14 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://1a563e9174d7d1c4b38e1ccde8a43250@o4507759113797632.ingest.us.sentry.io/4507759121989632",
+    dsn: "https://1a563e9174d7d1c4b38e1ccde8a43250@o4507759113797632.ingest.us.sentry.io/4507759121989632",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
+    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: process.env.NODE_ENV === 'development',
 });
