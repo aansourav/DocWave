cce9abff957ea79d56176f7a2c2b220e60b96a6b
aansourav <aansourav@gmail.com>
2024-08-11 17:47:23 +0600

    "Added Clerk user integration to LiveblocksProvider, including user resolution and mention suggestions"

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Loader from "@/components/Loader";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
     ClientSideSuspense,
     LiveblocksProvider,
@@ -8,8 +10,26 @@ import {
 import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+    const { user: clerkUser } = useUser();
+
     return (
-        <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <LiveblocksProvider
+            authEndpoint="/api/liveblocks-auth"
+            resolveUsers={async ({ userIds }) => {
+                const users = await getClerkUsers({ userIds });
+
+                return users;
+            }}
+            resolveMentionSuggestions={async ({ text, roomId }) => {
+                const roomUsers = await getDocumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+                    text,
+                });
+
+                return roomUsers;
+            }}
+        >
             <ClientSideSuspense fallback={<Loader />}>
                 {children}
             </ClientSideSuspense>
